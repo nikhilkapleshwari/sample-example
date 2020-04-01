3844aaae532be4dfee85221f8bc9ea129cfcf32b
nikhil1632@gmail.com <nikhil1632@gmail.com>
2020-04-01 23:52:31 +0530

    testing something

--- src/main/java/io/developer/app/Application.java
+++ src/main/java/io/developer/app/Application.java
@@ -3,7 +3,7 @@ package io.developer.app;
 public class Application {
 
     public static void main(String[] args) {
-	System.out.println("Hello World!");
+	System.out.println("Hello World! wassup?");
     }
 
 }
